72a6ed2b0685a17cefc0147551057863dd8161f4
Aeron Gabriel <76645232+AeronGabriel@users.noreply.github.com>
2023-12-14 22:32:32 +0800

    Add files via upload

---
+++ sectonTransition.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+// const sectionsLeft = document.querySelectorAll('.transition-left');
+// sectionsLeft.forEach((el) => observer.observe(el));
+
+// const sectionsRight = document.querySelectorAll('.transition-right');
+// sectionsRight.forEach((el) => observer.observe(el));
+
+const sections = document.querySelectorAll('.transition');
+sections.forEach((el) => observer.observe(el));
