08bbdfcb5e053d289fb563b4e8e849dfdd69cef5
Aeron Gabriel <76645232+AeronGabriel@users.noreply.github.com>
2024-02-15 22:03:27 +0800

    Update textAnimation.js

--- textAnimation.js
+++ textAnimation.js
@@ -2,10 +2,10 @@ const text = document.querySelector(".first-phrase");
 
 const textLoad = () => {
     setTimeout(() => {
-        text.textContent = "an Entry Level Web Developer";
+        text.textContent = "a Network Operation Center - L1 Analyst";
     }, 0);
     setTimeout(() => {
-        text.textContent = "an Entry Level Software Developer";
+        text.textContent = "an Entry Level Web Developer";
     }, 4000);
     setTimeout(() => {
         text.textContent = "a newbie Discord Bot Developer";
